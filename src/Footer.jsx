a96c87a67ba986b5c814a6e4416e060b43543088
Sathish K <sathishk1632@gmail.com>
2025-02-05 18:51:00 +0530

    Added routes

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+       <footer className="footer footer-center p-4 bg-blue-200  fixed bottom-0 text-black">
+  <aside>
+    <p>Copyright © 2025 - All right reserved by ACME Industries Ltd</p>
+  </aside>
+</footer>
+    </div>
+  )
+}
+
+export default Footer
